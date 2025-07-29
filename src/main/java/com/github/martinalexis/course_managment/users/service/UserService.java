574431ee50c1d16298dd7a7be697162ef1157a48
martin-alexis <alexismartin090@gmail.com>
2025-07-29 12:48:29 -0300

    feat(user): add user service
    This commit introduces the UserService to handle user-related operations.
    
    - Implement getUserById to retrieve users from the database.
    - Inject UserRepository dependency.
    - Handle potential "user not found" scenarios.

---
+++ src/main/java/com/github/martinalexis/course_managment/users/service/UserService.java
@@ -0,0 +1,19 @@
+package com.github.martinalexis.course_managment.users.service;
+
+import com.github.martinalexis.course_managment.users.model.UserModel;
+import com.github.martinalexis.course_managment.users.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserModel getUserById(int idUser) {
+        return userRepository.findById(idUser).orElseThrow();
+    }
+}
