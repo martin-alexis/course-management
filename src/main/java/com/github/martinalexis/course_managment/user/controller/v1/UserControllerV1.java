77a9fb7c515474fbb24db5d7736c5c60c43cc453
martin-alexis <alexismartin090@gmail.com>
2025-07-30 09:58:29 -0300

    refactor(user): update user API endpoint
    This commit updates the user API endpoint to follow REST API versioning best practices.
    
    - Update the request mapping to include the version.
    
    BREAKING CHANGE: The user API endpoint has been updated from `/api/users` to `/api/v1/users`.

--- src/main/java/com/github/martinalexis/course_managment/user/controller/v1/UserControllerV1.java
+++ src/main/java/com/github/martinalexis/course_managment/user/controller/v1/UserControllerV1.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/v1/users")
 public class UserControllerV1 {
 
     private final UserServiceV1 userServiceV1;
