1dca35d6d5cc49a84935e88ff28a434833eec3aa
martin-alexis <alexismartin090@gmail.com>
2025-08-16 12:17:30 -0300

    feat(user): add findByEmail method to UserRepository
    This commit introduces a new method to find users by their email address.
    
    - Add findByEmail method to UserRepository interface.
    - Use Optional to handle cases where the email is not found.

--- src/main/java/com/github/martinalexis/course_managment/user/repository/UserRepository.java
+++ src/main/java/com/github/martinalexis/course_managment/user/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.github.martinalexis.course_managment.user.repository;
 import com.github.martinalexis.course_managment.user.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserModel, Integer> {
+    Optional<UserModel> findByEmail(String email);
+
 }
