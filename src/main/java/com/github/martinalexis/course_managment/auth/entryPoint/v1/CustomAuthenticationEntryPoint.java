9516c09216c43779790c38362326bd7d04086c33
martin-alexis <alexismartin090@gmail.com>
2025-08-16 13:33:33 -0300

    feat(auth): implement custom authentication entry point
    Implement custom authentication entry point to handle unauthorized requests.
    
    - Return a ProblemDetail object for unauthorized requests.
    - Set the response status to 401 Unauthorized.
    - Set the response content type to application/problem+json.

---
+++ src/main/java/com/github/martinalexis/course_managment/auth/entryPoint/v1/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,37 @@
+package com.github.martinalexis.course_managment.auth.entryPoint.v1;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.URI;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+
+        ProblemDetail problem = ProblemDetail.forStatus(HttpStatus.UNAUTHORIZED);
+        problem.setTitle("Unauthorized");
+        problem.setDetail("You must provide a valid Bearer token to access this resource");
+        problem.setType(URI.create("https://example.com/errors/unauthorized"));
+        problem.setInstance(URI.create(request.getRequestURI()));
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/problem+json");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objectMapper.writeValue(response.getOutputStream(), problem);
+    }
+}
