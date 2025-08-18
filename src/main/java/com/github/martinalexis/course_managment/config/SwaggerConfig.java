e53e31210b377f536a98a65697a39774864c5f81
martin-alexis <alexismartin090@gmail.com>
2025-08-18 11:56:39 -0300

    feat(config): add Swagger configuration
    Configure Swagger/OpenAPI for API documentation and authorization.
    
    - Annotate with `@OpenAPIDefinition` for API metadata.
    - Define security scheme for JWT authentication.
    - Include development and production server URLs.

---
+++ src/main/java/com/github/martinalexis/course_managment/config/SwaggerConfig.java
@@ -0,0 +1,50 @@
+package com.github.martinalexis.course_managment.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.http.HttpHeaders;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Course Management API",
+                description = "API for managing courses, users, and authentication. " +
+                        "It supports local user registration and login (JWT) as well as OAuth2 providers (Google).",
+                version = "1.0.0",
+                contact = @Contact(
+                        name = "Alexis Martin",
+                        url = "https://github.com/martin-alexis"
+                )
+
+        ),
+        servers = {
+                @Server(
+                        description = "Development Server",
+                        url = "http://localhost:8080"
+                ),
+                @Server(
+                        description = "Production Server",
+                        url = "-"
+                )
+        },
+        security = @SecurityRequirement(
+                name = "Bearer Authentication"
+        )
+)
+@SecurityScheme(
+        name = "Bearer Authentication",
+        description = "A JWT Token is required to access protected endpoints. " +
+                "Obtain the token from the login endpoint and paste it here in the format 'Bearer {token}'.",
+        type = SecuritySchemeType.HTTP,
+        paramName = HttpHeaders.AUTHORIZATION,
+        in = SecuritySchemeIn.HEADER,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class SwaggerConfig {}
