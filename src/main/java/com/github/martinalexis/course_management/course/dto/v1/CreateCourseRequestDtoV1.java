8952b4e12921d27cae607810944170c5d76d504f
martin-alexis <alexismartin090@gmail.com>
2025-09-17 13:13:07 -0300

    feat(course): add CreateCourse DTOs
    - Create request DTO for creating courses
    - Create response DTO to return course details

---
+++ src/main/java/com/github/martinalexis/course_management/course/dto/v1/CreateCourseRequestDtoV1.java
@@ -0,0 +1,6 @@
+package com.github.martinalexis.course_management.course.dto.v1;
+
+public class CreateCourseRequestDtoV1 {
+    private String title;
+    private String description;
+   }
---
+++ src/main/java/com/github/martinalexis/course_management/course/dto/v1/CreateCourseResponseDtoV1.java
@@ -0,0 +1,10 @@
+package com.github.martinalexis.course_management.course.dto.v1;
+
+import java.time.LocalDateTime;
+
+public class CreateCourseResponseDtoV1 {
+    private Integer idCourse;
+    private String title;
+    private String description;
+    private LocalDateTime CretedOn;
+   }
