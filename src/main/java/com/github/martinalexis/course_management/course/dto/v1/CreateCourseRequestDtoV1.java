2bd1f8d9a55a47ce16d8355cc410b20ac7bda6e0
martin-alexis <alexismartin090@gmail.com>
2025-09-17 13:28:38 -0300

    feat(course): add lombok annotations to DTOs
    - Add @Data, @AllArgsConstructor and @NoArgsConstructor to CreateCourseRequestDtoV1
    - Add @Data, @AllArgsConstructor and @NoArgsConstructor to CreateCourseResponseDtoV1

--- src/main/java/com/github/martinalexis/course_management/course/dto/v1/CreateCourseRequestDtoV1.java
+++ src/main/java/com/github/martinalexis/course_management/course/dto/v1/CreateCourseRequestDtoV1.java
@@ -1,5 +1,12 @@
 package com.github.martinalexis.course_management.course.dto.v1;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
 public class CreateCourseRequestDtoV1 {
     private String title;
     private String description;
--- src/main/java/com/github/martinalexis/course_management/course/dto/v1/CreateCourseResponseDtoV1.java
+++ src/main/java/com/github/martinalexis/course_management/course/dto/v1/CreateCourseResponseDtoV1.java
@@ -1,7 +1,14 @@
 package com.github.martinalexis.course_management.course.dto.v1;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
 public class CreateCourseResponseDtoV1 {
     private Integer idCourse;
     private String title;
