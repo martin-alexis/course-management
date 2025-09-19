5f25640f096e767a2183e8db977e9cc9aeec7395
martin-alexis <alexismartin090@gmail.com>
2025-09-19 12:42:46 -0300

    feat(role): Add findByRole method to RoleRepository
    - Implement method to find roles by enum

--- src/main/java/com/github/martinalexis/course_management/course/repository/RoleRepository.java
+++ src/main/java/com/github/martinalexis/course_management/course/repository/RoleRepository.java
@@ -1,7 +1,11 @@
 package com.github.martinalexis.course_management.course.repository;
 
+import com.github.martinalexis.course_management.course.model.RoleEnum;
 import com.github.martinalexis.course_management.course.model.RoleModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<RoleModel, Integer> {
+    Optional<RoleModel> findByRole(RoleEnum roleEnum);
 }
