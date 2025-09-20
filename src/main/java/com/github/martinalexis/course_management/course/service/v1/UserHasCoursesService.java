2f0f19c77117cb0a40adddc25d13725d7fed8cd1
martin-alexis <alexismartin090@gmail.com>
2025-09-20 13:35:24 -0300

    feat(course): Implement user enrollment and role assignment
    - Add services for role management and user-course relations.
    - Implement rules to prevent invalid enrollments.

---
+++ src/main/java/com/github/martinalexis/course_management/course/service/v1/UserHasCoursesService.java
@@ -0,0 +1,55 @@
+package com.github.martinalexis.course_management.course.service.v1;
+
+import com.github.martinalexis.course_management.common.exceptions.ResourceNotFoundException;
+import com.github.martinalexis.course_management.course.model.CourseModel;
+import com.github.martinalexis.course_management.course.model.RoleEnum;
+import com.github.martinalexis.course_management.course.model.RoleModel;
+import com.github.martinalexis.course_management.course.model.UserHasCoursesModel;
+import com.github.martinalexis.course_management.course.repository.RoleRepository;
+import com.github.martinalexis.course_management.course.repository.UserHasCoursesRepository;
+import com.github.martinalexis.course_management.user.model.UserModel;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class UserHasCoursesService {
+
+    private final UserHasCoursesRepository userHasCoursesRepository;
+    private final RoleService roleService;
+
+
+    @Transactional
+    public UserHasCoursesModel assignTeacherToCourse(UserModel user, CourseModel savedCourse) {
+
+        RoleModel role = roleService.getRoleOrThrow(RoleEnum.TEACHER);
+
+        UserHasCoursesModel assignTeacher = new UserHasCoursesModel();
+        assignTeacher.setUsersId(user);
+        assignTeacher.setCoursesId(savedCourse);
+        assignTeacher.setRolesId(role);
+        assignTeacher.setInscriptionDate(savedCourse.getCreatedOn());
+
+        return userHasCoursesRepository.save(assignTeacher);
+    }
+
+    @Transactional
+    public UserHasCoursesModel enrollStudent(UserModel user, CourseModel course) {
+
+        RoleModel role = roleService.getRoleOrThrow(RoleEnum.STUDENT);
+
+        UserHasCoursesModel enrollment = new UserHasCoursesModel();
+        enrollment.setUsersId(user);
+        enrollment.setCoursesId(course);
+        enrollment.setRolesId(role);
+        enrollment.setInscriptionDate(LocalDateTime.now());
+
+        return userHasCoursesRepository.save(enrollment);
+    }
+
+
+
+}
