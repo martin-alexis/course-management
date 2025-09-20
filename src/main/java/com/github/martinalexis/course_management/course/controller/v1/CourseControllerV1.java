bd1f790f6565563c4f1f5b9fe8f6d4da8f9c3a8c
martin-alexis <alexismartin090@gmail.com>
2025-09-20 13:36:54 -0300

    feat(course): Implement course creation and enrollment
    - Delegate course creation to CourseUseCase.
    - Add endpoint for student enrollment.

--- src/main/java/com/github/martinalexis/course_management/course/controller/v1/CourseControllerV1.java
+++ src/main/java/com/github/martinalexis/course_management/course/controller/v1/CourseControllerV1.java
@@ -2,7 +2,8 @@ package com.github.martinalexis.course_management.course.controller.v1;
 
 import com.github.martinalexis.course_management.course.dto.v1.CreateCourseRequestDtoV1;
 import com.github.martinalexis.course_management.course.dto.v1.CreateCourseResponseDtoV1;
-import com.github.martinalexis.course_management.course.service.v1.CourseServiceV1;
+import com.github.martinalexis.course_management.course.dto.v1.EnrollCourseResponseDtoV1;
+import com.github.martinalexis.course_management.course.service.v1.facade.CourseUseCase;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,12 +14,15 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/courses")
 public class CourseControllerV1 {
-    private final CourseServiceV1 courseService;
+    private final CourseUseCase courseUseCase;
 
     @PostMapping
     public ResponseEntity<CreateCourseResponseDtoV1> createCourse(@Valid @RequestBody CreateCourseRequestDtoV1 request) {
-        CreateCourseResponseDtoV1 course = courseService.createCourse(request);
-        return ResponseEntity.status(HttpStatus.CREATED).body(course);
+        return ResponseEntity.status(HttpStatus.CREATED).body(courseUseCase.createCourse(request));
     }
 
+    @PostMapping("/{idCourse}/enroll")
+    public ResponseEntity<EnrollCourseResponseDtoV1> enrollingStudentToCourse(@PathVariable int idCourse) {
+        return ResponseEntity.status(HttpStatus.OK).body(courseUseCase.enrollStudentToCourse(idCourse));
+    }
 }
