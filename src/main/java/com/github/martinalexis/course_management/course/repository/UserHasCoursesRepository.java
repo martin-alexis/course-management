31e9f66807b259f7af5819edb4c2b0ed62aa87c6
martin-alexis <alexismartin090@gmail.com>
2025-09-20 13:30:57 -0300

    feat(UserHasCourses): Add find by user and course
    - Implement query to find by user and course

--- src/main/java/com/github/martinalexis/course_management/course/repository/UserHasCoursesRepository.java
+++ src/main/java/com/github/martinalexis/course_management/course/repository/UserHasCoursesRepository.java
@@ -1,7 +1,13 @@
 package com.github.martinalexis.course_management.course.repository;
 
+import com.github.martinalexis.course_management.course.model.CourseModel;
 import com.github.martinalexis.course_management.course.model.UserHasCoursesModel;
+import com.github.martinalexis.course_management.user.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserHasCoursesRepository extends JpaRepository<UserHasCoursesModel, Integer> {
+
+    Optional<UserHasCoursesModel> findByUsersIdAndCoursesId(UserModel user, CourseModel idCourse);
 }
