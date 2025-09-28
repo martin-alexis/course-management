fee4b11df3389af454b5848de0a65477cb1683d7
martin-alexis <alexismartin090@gmail.com>
2025-09-27 22:34:10 -0300

    feat(lesson): Implement LessonMapper interface
    - Add lessonRequestTtoEntity method

---
+++ src/main/java/com/github/martinalexis/course_management/lesson/mapper/v1/LessonMapper.java
@@ -0,0 +1,11 @@
+package com.github.martinalexis.course_management.lesson.mapper.v1;
+
+import com.github.martinalexis.course_management.lesson.dto.v1.LessonRequestDto;
+import com.github.martinalexis.course_management.lesson.model.LessonModel;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface LessonMapper {
+    // Request -> Entity
+    LessonModel lessonRequestTtoEntity(LessonRequestDto dto);
+}
