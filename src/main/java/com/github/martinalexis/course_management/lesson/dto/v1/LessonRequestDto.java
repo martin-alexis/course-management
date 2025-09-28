2f19ad37ace2745fc335901ea0bdf9ab84c96f2e
martin-alexis <alexismartin090@gmail.com>
2025-09-27 22:28:57 -0300

    feat(lesson): implement LessonRequestDto
    Add LessonRequestDto class for handling lesson creation requests.
    
    * Create `LessonRequestDto.java` file.
    * Define an empty class `LessonRequestDto`.
    * Add package declaration.

---
+++ src/main/java/com/github/martinalexis/course_management/lesson/dto/v1/LessonRequestDto.java
@@ -0,0 +1,34 @@
+package com.github.martinalexis.course_management.lesson.dto.v1;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.URL;
+
+import java.time.LocalDateTime;
+
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class LessonRequestDto {
+
+    @NotBlank(message = "Title is mandatory")
+    @Schema(description = "Title of the lesson", example = "Introduction to Java", required = true)
+    private String title;
+
+    @NotBlank(message = "Description is mandatory")
+    @Size(max = 255, message = "Description must be at most 255 characters")
+    @Schema(description = "Description of the lesson", example = "Learn Java basics and OOP concepts", required = true)
+    private String description;
+
+    @URL(message = "Video URL must be a valid URL")
+    @Schema(description = "URL of the lesson video", example = "https://www.youtube.com/watch?v=someVideoId", required = true)
+    private String videoUrl;
+
+    @Schema(description = "Publication date of the lesson", example = "2025-09-10T10:15:30")
+    private LocalDateTime publishedOn;
+}
