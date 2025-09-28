096d679ed72e587c38b893110e89dea1f99bace9
martin-alexis <alexismartin090@gmail.com>
2025-09-27 23:21:37 -0300

    fix(lesson): Use correct DTO for entity to response mapping
    - Fix return type of lessonEntityToResponse to LessonResponseDto

--- src/main/java/com/github/martinalexis/course_management/lesson/mapper/v1/LessonMapper.java
+++ src/main/java/com/github/martinalexis/course_management/lesson/mapper/v1/LessonMapper.java
@@ -1,6 +1,7 @@
 package com.github.martinalexis.course_management.lesson.mapper.v1;
 
 import com.github.martinalexis.course_management.lesson.dto.v1.LessonRequestDto;
+import com.github.martinalexis.course_management.lesson.dto.v1.LessonResponseDto;
 import com.github.martinalexis.course_management.lesson.model.LessonModel;
 import org.mapstruct.Mapper;
 
@@ -9,5 +10,5 @@ public interface LessonMapper {
     // Request -> Entity
     LessonModel lessonRequestTtoEntity(LessonRequestDto dto);
     // Entity -> Response
-    LessonRequestDto lessonEntityToResponse(LessonModel entity);
+    LessonResponseDto lessonEntityToResponse(LessonModel entity);
 }
