82f378bbb4d47aa51331521e14dc8ceaf96528a1
martin-alexis <alexismartin090@gmail.com>
2025-10-02 18:54:07 -0300

    feat(course): Add UpdateCourseRequestDto and mapper method
    - Create UpdateCourseRequestDto for updating courses
    - Add mapper method to map dto to entity

--- src/main/java/com/github/martinalexis/course_management/course/mapper/v1/CourseMapperV1.java
+++ src/main/java/com/github/martinalexis/course_management/course/mapper/v1/CourseMapperV1.java
@@ -3,6 +3,7 @@ package com.github.martinalexis.course_management.course.mapper.v1;
 import com.github.martinalexis.course_management.course.dto.v1.CreateCourseRequestDtoV1;
 import com.github.martinalexis.course_management.course.dto.v1.CreateCourseResponseDtoV1;
 import com.github.martinalexis.course_management.course.dto.v1.EnrollCourseResponseDtoV1;
+import com.github.martinalexis.course_management.course.dto.v1.UpdateCourseRequestDto;
 import com.github.martinalexis.course_management.course.model.CourseModel;
 import org.mapstruct.Mapper;
 
@@ -10,6 +11,8 @@ import org.mapstruct.Mapper;
 public interface CourseMapperV1 {
     // Request -> Entity
     CourseModel toEntity(CreateCourseRequestDtoV1 dto);
+
+    CourseModel updateCourseRequestToEntity(UpdateCourseRequestDto dto);
 
     // Entity -> Response
     CreateCourseResponseDtoV1 toResponse(CourseModel course);
