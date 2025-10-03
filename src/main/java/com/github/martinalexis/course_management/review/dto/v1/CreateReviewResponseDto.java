e39e3a85d7bf1ca0912abdc20f456a8a43f30e75
martin-alexis <alexismartin090@gmail.com>
2025-10-03 16:33:40 -0300

    feat(review): add review DTOs v1
    Add CreateReviewRequestDto and CreateReviewResponseDto
    Implement validation constraints and schema descriptions

---
+++ src/main/java/com/github/martinalexis/course_management/review/dto/v1/CreateReviewResponseDto.java
@@ -0,0 +1,24 @@
+package com.github.martinalexis.course_management.review.dto.v1;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class CreateReviewResponseDto {
+
+    @Schema(description = "Optional comment for the review.", example = "This course was amazing, I learned a lot!")
+    private String comment;
+
+    @Schema(description = "The score given to the course, from 1 to 5.", example = "5")
+    private Integer score;
+
+    @Schema(description = "The timestamp when the review was created.", example = "2023-10-27T10:00:00")
+    private LocalDateTime createdOn;
+
+}
